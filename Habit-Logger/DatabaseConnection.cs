b0b5b86d66feca8220f36d7f40bebb20d1071bff
Codersauruse <shehandesk123@gmail.com>
2024-07-02 20:00:19 +0530

    new version of Habbit tracker

---
+++ Habit-Logger/DatabaseConnection.cs
@@ -0,0 +1,153 @@
+﻿using ConsoleTableExt;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Habit_Logger
+{
+    internal class DatabaseConnection
+    {
+        private readonly string _connectionString;
+        private string query;
+        private MySqlDataAdapter da;
+
+        public DatabaseConnection()
+        {
+            _connectionString = "datasource=localhost;port=3306;database=habits;user=code;password=code123";
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+
+
+        public void insertdata(String habitName,DateTime startDate,int hours) { 
+            using(MySqlConnection conn = GetConnection())
+            {
+                query = "INSERT INTO habits (habitName, startDate,endDate, hours ,progress) VALUES (@habitName, @startDate, @endDate, @hours ,@progress)";
+                using(var cmd = new MySqlCommand(query, conn)) {
+                    cmd.Parameters.AddWithValue("@habitName", habitName);
+                    cmd.Parameters.AddWithValue("@startDate", startDate);
+                    cmd.Parameters.AddWithValue("@endDate", DBNull.Value); // Assuming endDate can be NULL
+                    cmd.Parameters.AddWithValue("@hours", hours);
+
+                    cmd.Parameters.AddWithValue("@progress", 1);
+
+                    try { 
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        //Console.WriteLine("Record added Successfully");
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Record isn't added");
+
+                    }
+
+
+                }
+            }
+            
+        
+        }
+        public void deletedata(String habitName) {
+
+            query = "DELETE FROM habits WHERE habitName = @habitName";
+
+            using (var connection = GetConnection())
+            {
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@habitName", habitName);
+
+                    try
+                    {
+                        connection.Open();
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine("record deleted successfully");
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error deleting data: {ex.Message}");
+                    }
+                }
+            }
+        }
+        public void updateData(String habitName,DateTime endDate,int progress) {
+            using (MySqlConnection conn = GetConnection())
+            {
+                query = "UPDATE habits SET endDate = @endDate,progress = @progress WHERE habitName = @habitName;";
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@habitName", habitName);
+                    cmd.Parameters.AddWithValue("@endDate", endDate);
+                    cmd.Parameters.AddWithValue("@progress", progress);
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine("Record updated Successfully");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Record isn't updated");
+
+                    }
+
+
+                }
+            }
+
+
+        }
+
+        public void ViewData() {
+            query = "SELECT * FROM habits";
+
+            using (var connection = GetConnection())
+            {
+                
+                   
+
+                    try
+                    {
+                        connection.Open();
+                        da = new MySqlDataAdapter(query,connection);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                         var tableData = new List<List<object>>();
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        
+                        tableData.Add(new List<object> { row["habitName"], row["startDate"], row["endDate"], row["hours"], row["progress"] });
+                       
+                    }
+                    ConsoleTableBuilder
+                    .From(tableData)
+                    .ExportAndWriteLine();
+
+                }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error deleting data: {ex.Message}");
+                    }
+                
+            }
+
+        }  
+
+    }
+
+}
+
---
+++ Habit-Logger/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Habit_Logger
+{
+    internal class Program
+    {
+        private static String habitName;
+        private static String year, month, day, time;
+        private  static DatabaseConnection db  = new DatabaseConnection();
+        static void Main(string[] args)
+        {
+            App();
+            
+
+        }
+        //Basic running application.
+        public static void  App()
+        {
+            String userInput;
+            Console.WriteLine("..........Hello Welcome To The Habbit Logger..........\n\n");
+            Console.WriteLine("MAIN MENU\n\n");
+            while (true) 
+            {
+                
+                Console.WriteLine("What do you like to do :");
+                Console.WriteLine("--------------------------\n");
+                Console.WriteLine("Type \"0\" to exit :\nType \"v\" to View All Habits :\n" +
+                    "Type \"a\" to add a Habit :\nType \"d\" to delete a Habit :\n"+
+                  "Type \"u\" to update a Habit :\n\n");
+                Console.Write("Enter your choice :");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    case "a":
+                        addhabit();
+                        break;
+                    case "d":
+                        deletehabit();
+                        break;
+                    case "u":
+                        updatehabit();
+                        break;
+                    case "v":
+                        viewhabit();
+                        break;
+
+                }
+
+            }
+
+        }
+        public static void addhabit() {
+            Console.Write("enter the habit name :");
+            habitName = Console.ReadLine();
+            Console.Write("enter the current year :");
+            year = Console.ReadLine();
+            Console.Write("enter the current month :");
+            month = Console.ReadLine();
+            Console.Write("enter the current day :");
+            day = Console.ReadLine();
+            Console.Write("enter the Allocated time for the habit in minutes(ex:- 60) :");
+            time = Console.ReadLine();
+            DateTime startDate = new DateTime(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day));
+            db.insertdata(habitName,startDate,Convert.ToInt32(time));
+
+
+
+
+        }
+        public static void deletehabit() {
+            Console.Write("enter the habit name :");
+            habitName = Console.ReadLine();
+            db.deletedata(habitName);
+
+        }
+        public static void updatehabit() {
+            bool flag = false;
+         
+                Console.Write("enter the habit name :");
+                habitName = Console.ReadLine();
+
+            Console.Write("Do you want to end the habit (yes or no):");
+            string desc  = Console.ReadLine();
+            flag =  (desc.Equals("yes"))? true : false;
+            DateTime endDate;
+            int progress = 0;
+            if (flag)
+            {
+                
+                endDate = DateTime.Now;
+                db.updateData(habitName, endDate, progress);
+               
+                
+            }
+            
+
+        }
+
+        public static void viewhabit() {
+            db.ViewData();
+        
+        }
+    }
+    
+}
